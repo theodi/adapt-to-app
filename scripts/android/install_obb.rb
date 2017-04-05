8dd2b01944224c73e2bc8c64b98707db37c70be8
Jez Higgins <jez@jezuk.co.uk>
2017-04-05 15:11:04 +0100

    Use adb to tell use the EXTERNAL_STORAGE mount point, so we can push the OBB into the right place

--- scripts/android/install_obb.rb
+++ scripts/android/install_obb.rb
@@ -10,11 +10,17 @@ platformRoot = pwd + "/platforms/android"
 buildDir = platformRoot + "/build/obb"
 
 androidDir = ENV['ANDROID_HOME']
+adb = "#{androidDir}/platform-tools/adb"
 
 puts "Installing OBB to device ... " if wordy
 obbFileName = "main.1.#{apkName}.obb"
 
-adbcmd = "#{androidDir}/platform-tools/adb push #{buildDir}/#{obbFileName} /sdcard/Android/obb/#{apkName}/#{obbFileName}"
-puts "  running #{adbcmd}" if wordy
-`#{adbcmd}`
+mntpointcmd = "#{adb} shell echo \\$EXTERNAL_STORAGE"
+puts "  finding sdcard mount point with #{mntpointcmd}" if wordy
+mntpoint = `#{mntpointcmd}`.chomp
+puts "  mount point is #{mntpoint}" if wordy
+
+pushcmd = "#{adb} push #{buildDir}/#{obbFileName} #{mntpoint}/Android/obb/#{apkName}/#{obbFileName}"
+puts "  running #{pushcmd}" if wordy
+`#{pushcmd}`
 puts "Expansion file installed to /sdcard/Android/obb/#{apkName}/#{obbFileName}"
