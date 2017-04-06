a26fa92ed1a8d0d42b97868fd902b218f50d1fb6
Jez Higgins <jez@jezuk.co.uk>
2017-04-06 13:24:18 +0100

    Verify Java is available and at least 1.8

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 "use strict";
 
+const java_version = require("./lib/java_version");
+java_version.check();
+
+
 const android_install = require("./lib/android_install");
 android_install.check();
