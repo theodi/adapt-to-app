5b3104b6fc6394411c532d70755e6f48201af9e7
Jez Higgins <jez@jezuk.co.uk>
2017-04-06 17:08:21 +0100

    Made everything more promisey

--- index.js
+++ index.js
@@ -2,31 +2,36 @@
 "use strict";
 
 const java_version = require("./lib/java_version");
-java_version.check();
-
 const android_install = require("./lib/android_install");
-android_install.check();
 
-const buildDir = process.cwd();
-
-// unzip package here
-// parse out config
+const fs = require('fs-extra');
+const path = require('path');
 
 const macosx = (process.platform === "darwin");
-
+const buildDir = process.cwd();
 const appDir = "app";
-const appId = "org.theodi.megavision";
-const appName = "MegaVision";
 
-cordova_create(appDir, appId, appName);
-cordova_build("android");
-cordova_build("ios");
+//build_app();
+java_version.check().
+    then(android_install.check).
+    then(build_app);
+
+function build_app() {
+    // unzip package here
+    // parse out config
+
+    const appId = "org.theodi.megavision";
+    const appName = "MegaVision";
+
+    cordova_create(appDir, appId, appName);
+    cordova_build("android");
+    cordova_build("ios");
+} // build_app
+
 
 ////////////////////////////////////
 function cordova_create(appDir, appId, appName) {
-    const fs = require('fs');
     if (fs.existsSync(appDir)) {
-	console.log("woo");
 	change_to_appDir();
 	return;
     } // if ...
@@ -43,17 +48,40 @@ function cordova_build(platform) {
     if (!macosx && (platform === "ios"))
 	return;
     cordova("build", platform);
+
+    // find build product
+    const suffix = {"android": ".apk", "ios": ".trousers"};
+    const app = find_app(suffix[platform]);
+    if (!app) {
+	console.log("\n\nCould not find " + platform + " app!\n\n");
+	return;
+    } //
+
+    fs.moveSync(app, path.join(buildDir, path.basename(app)), true);
+    console.log("\n\nBuilt " + path.basename(app));
 } // cordova_build
 
 function cordova(command, ...options) {
     const child_process = require('child_process');
-    const prefix = buildDir + "/node_modules/cordova/bin/";
+    const prefix = path.join(buildDir, "/node_modules/cordova/bin/");
     const cmd = "cordova " + command + " " + options.join(" ");
     console.log(cmd);
     child_process.execSync(prefix + cmd, { stdio: 'inherit' })
 } // cordova
 
-
+function find_app(suffix, dir = process.cwd() + "/platforms") {
+    for (const name of fs.readdirSync(dir)) {
+	const fullName = path.join(dir, name);
+	const stat = fs.statSync(fullName);
+	if (stat.isFile() && name.endsWith(suffix))
+	    return fullName;
+	if (stat.isDirectory()) {
+	    let app = find_app(suffix, fullName);
+	    if (app)
+		return app;
+	} // if ...
+    }
+} // find_app
 
 function change_to_appDir() {
     process.chdir(appDir);
