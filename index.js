5ca6a631ca3cd6ea056dbcab6878b50801359e18
Jez Higgins <jez@jezuk.co.uk>
2017-04-07 09:08:44 +0100

    Start unwrapping the Adapt zipfile

--- index.js
+++ index.js
@@ -8,14 +8,78 @@ const fs = require('fs-extra');
 const path = require('path');
 
 const macosx = (process.platform === "darwin");
-const buildDir = process.cwd();
-const appDir = "app";
+const buildDir = path.dirname(process.argv[1])
+const tmpDir = path.join(buildDir, "tmp");
+const appDir = path.join(buildDir, "app");
 
-//build_app();
-java_version.check().
-    then(android_install.check).
-    then(build_app);
+unwrap_zip_file(process.argv).
+    then(verify_zip_file).
+    catch((error) => console.log(error));
 
+//java_version.check().
+//    then(android_install.check).
+//    then(build_app);
+
+////////////////////////////////////////////
+function unwrap_zip_file(args) {
+    const p = new Promise((resolve, reject) => {
+	if (args.length != 3)
+	    reject("Usage: " + path.basename(args[1]) + " <adapt-zip-file>\n" +
+		   (args.length < 3 ? "Must pass a zip filename" : "Easy Tiger! One zipfile at time!"));
+
+	const source_file = args[2];
+	if (!fs.existsSync(source_file))
+	    reject("The file " + source_file + " does not exist.");
+
+	clean_tmp_directory();
+	const extract = require('extract-zip');
+	extract(source_file, {dir: tmpDir}, (error) => {
+	    if (!error) {
+		console.log("Unzipped " + source_file + " into " + tmpDir);
+		resolve();
+	    } else {
+		reject(error)
+	    }
+	});
+    });
+    return p;
+} // unwrap_zip_file
+
+function clean_tmp_directory() {
+    if (!fs.existsSync(tmpDir)) {
+	fs.mkdirSync(tmpDir);
+	return;
+    } // if ...
+
+    clean_directory(tmpDir);
+} // clean_tmp_directory
+
+function clean_directory(dir) {
+    for (const name of fs.readdirSync(dir)) {
+	const fullName = path.join(dir, name);
+	const stat = fs.statSync(fullName);
+	if (stat.isFile())
+	    fs.unlinkSync(fullName);
+	if (stat.isDirectory()) {
+	    clean_directory(fullName);
+	    fs.rmdirSync(fullName);
+	} // if ...
+    } // for ...
+} // clean_directory
+
+function verify_zip_file() {
+    const p = new Promise((resolve, reject) => {
+	const check_files = ["index.html", "adapt", "course/config.json", "course/en/course.json"];
+	for (const c of check_files)
+	    if (!fs.existsSync(path.join(tmpDir, c)))
+		reject("Doesn't look like an Adapt zipfile.  Could not find file " + c);
+	resolve();
+    });
+    return p;
+} // verify_zip_file
+
+/////////////////////////////////////
+/////////////////////////////////////
 function build_app() {
     // unzip package here
     // parse out config
@@ -27,7 +91,6 @@ function build_app() {
     cordova_build("android");
     cordova_build("ios");
 } // build_app
-
 
 ////////////////////////////////////
 function cordova_create(appDir, appId, appName) {
